f602159e413b33cac248c18133054f48691f92a8
PhamBinh123-Au <phambinh2092008@gmail.com>
2025-12-31 23:09:16 +0700

    Update future date in app.js to Dec 31, 2025

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var fut = new Date("Dec 31, 2026 11:09:00").getTime()
+var fut = new Date("Dec 31, 2025 11:10:00").getTime()
     let x = setInterval(function stime(){
     var now = new Date().getTime()
     var D = fut - now
